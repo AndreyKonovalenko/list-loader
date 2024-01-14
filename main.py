4f2bc9626283afe02ce0dadc1c9496481f1d2459
AndreyKonovalenko <andreinew@mail.ru>
2024-01-14 22:19:01 +0300

    chore: install selenium

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 
 import os
-print(os.environ['login'])
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+# print(os.environ['login'])
+
+browser = webdriver.Firefox()
+browser.get('https://fedsfm.ru')
+try:
+   browser.find_element(By.CLASS_NAME, 'btn.btn-danger').click()
+except:
+   print("Can't find element")
