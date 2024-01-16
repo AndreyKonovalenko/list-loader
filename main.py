00338816a659880d2579ed75f3e1a154180e8516
Andrey Konovalenko <andreinew@mail.ru>
2024-01-16 17:24:44 +0300

    chore: main functioality updated

--- main.py
+++ main.py
@@ -20,26 +20,32 @@ browser.get(base_location)
 
 current_location = browser.current_url
 
-# def clickByID(id: str) -> None:
-#      try:
-#           browser.find_element(By.ID, id).click()
-#      except:
-#           print(f'Element with id: {id} not found/')
+def delay_click(element: any) -> None:
+      wait = WebDriverWait(browser, timeout=3)
+      wait.until(lambda d : element.is_displayed())
+      element.click()
+     
+
+def clickByID(id: str) -> None:
+   try:
+      element =  browser.find_element(By.ID, id)
+      delay_click(element)   
+   except:
+      print(f'Element with id: {id} not found/')
 
 
-# def delay(base_location, current_location): 
-#      count = 0
-#      while(base_location == current_location):
-#          print(current_location)
-#          time.sleep(1)
-#          print('waiting')
-#          count += 1
-#          if count == maxDelay:
-#               print("maximum number of attempts reached")
-#               break
-#          if base_location != current_location:
-#               base_location = current_location
-#               break         
+def delay(base_location: str, current_location: str) -> None : 
+     count = 0
+     while(base_location == current_location):
+         time.sleep(1)
+         current_location = browser.current_url
+         count += 1
+         if count == maxDelay:
+              print("maximum number of attempts reached")
+              break
+         if base_location != current_location:
+              base_location = current_location
+              break         
 
 
 def loginHandler() -> None:
@@ -49,45 +55,22 @@ def loginHandler() -> None:
       loginInput.send_keys(login)
       passwordInput.send_keys(password) 
       loginButton = browser.find_element(By.ID, 'loginButton')
-      wait = WebDriverWait(browser, timeout=3)
-      wait.until(lambda d : loginButton.is_displayed())
-      loginButton.click()
+      delay_click(loginButton)
    except: 
       print("login failed!")
       browser.close()
 
-# def download() -> None:
-#      browser.find_element(By.XPATH, '//a[text()="Скачать"]').click()         
+def download() -> None:
+      downloadLink = browser.find_element(By.XPATH, '//a[text()="Скачать"]')
+      delay_click(downloadLink)      
 
 loginHandler()
-# delay(base_location, current_location)
 
-# # for id in elementIdList: 
-# #       delay(base_location, current_location)
-# #       clickByID(id)
-      
+delay(base_location, current_location)
 
+for id in elementIdList: 
+   delay(base_location, current_location)
+   clickByID(id)
 
-
-# try:
-#      time.sleep(2)
-#      browser.find_element(By.CLASS_NAME, 'btn.btn-danger').click()
-#      time.sleep(2)
-#      loginInput = browser.find_element(By.ID, 'loginEditor')
-#      passwordInput = browser.find_element(By.ID, 'passwordEditor')
-#      loginInput.send_keys(login)
-#      passwordInput.send_keys(password) 
-#      browser.find_element(By.ID, 'loginButton').click()
-#      time.sleep(5)
-#      print(browser.current_url)
-#      browser.find_element(By.ID, 'e91ae743-b77c-45a7-b5d6-3444e8c60f86').click()
-#      time.sleep(2) 
-#      print(browser.current_url)  
-#      browser.find_element(By.ID, '8d0a3196-062d-4987-8ea0-90efddba53fc_anchor').click()
-#      print(browser.current_url)
-#      time.sleep(2)
-#      browser.find_element(By.XPATH, '//a[text()="Скачать"]').click()
-# except:
-#    print("Can't find element")
-#    time.sleep(5)
-#    browser.close()
+time.sleep(2)
+download()
