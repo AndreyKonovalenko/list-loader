29cc23c587ce0570ae6244acfba8c305ca1d4ad3
Andrey Konovalenko <andreinew@mail.ru>
2024-01-18 11:18:40 +0300

    feat: declaration needed list type in python script second argument: t*: o*, m*

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
 
 import os
+import sys
 import time
 from dotenv import load_dotenv 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 
+list_types = ('ter', "oon", "mvk")
+
+if len(sys.argv) < 2:
+    print('provide argrument with list type')
+    exit()
+
+if (sys.argv[1] in list_types) == False:
+     print('Incorrect list type')
+     exit()
+    
 load_dotenv()
 
-elementIdList = ["e91ae743-b77c-45a7-b5d6-3444e8c60f86", "e91ae743-b77c-45a7-b5d6-3444e8c60f86", "8d0a3196-062d-4987-8ea0-90efddba53fc_anchor"]
+elementIdList = ["e91ae743-b77c-45a7-b5d6-3444e8c60f86", "e91ae743-b77c-45a7-b5d6-3444e8c60f86"]
+
+# "8d0a3196-062d-4987-8ea0-90efddba53fc_anchor"
 
 login = os.environ['login']
 password = os.environ['password']
@@ -61,7 +74,7 @@ def loginHandler() -> None:
       browser.close()
 
 def download() -> None:
-      downloadLink = browser.find_element(By.XPATH, '//a[text()="Скачать"]')
+      downloadLink = browser.find_element(By.XPATH, '//a[@href="/SkedDownload/GetActiveSked?type=xml21"]')
       delay_click(downloadLink)      
 
 loginHandler()
@@ -72,5 +85,4 @@ for id in elementIdList:
    delay(base_location, current_location)
    clickByID(id)
 
-time.sleep(2)
 download()
