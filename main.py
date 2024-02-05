4711c54b7928fba415e1186854e0fa8fe3ae98d7
Andrey Konovalenko <andreinew@mail.ru>
2024-02-05 11:29:29 +0300

    chore: added upzip functionality

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from dotenv import load_dotenv
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
+from zipfile import ZipFile
+
 
 from browser_module import browser
 
@@ -20,6 +22,7 @@ formIdList = ['loginEditor', 'passwordEditor']
 formInputs = [login, password]
 elementIdList = ["e91ae743-b77c-45a7-b5d6-3444e8c60f86"]
 browser.get(url)
+directory = os.environ['directory']
 
 current_location = browser.current_url
 
@@ -59,18 +62,38 @@ def download(listType) -> None:
       wait_to_be_displayed(downloadLink) 
       downloadLink.click()   
 
+def unzipDownoladed():
+   for file in os.listdir(directory):
+      if file.endswith(".zip"):
+         try:
+            zipFilePath = os.path.join(directory, file)
+            zip_ref = ZipFile(zipFilePath, 'r')
+            zip_ref.extractall(directory)
+            zip_ref.close()
+         except:
+             print("Extracting failed")
+             exit()
+
+def clearDirectory():
+   for file in os.listdir(directory):
+      if file.endswith(".zip"):
+         try:
+            zipFilePath = os.path.join(directory, file)
+            os.remove(zipFilePath)
+            print(f'{file} succesfule removed')
+         except:
+            print("Directory cliaring failed")
+
 
 if len(sys.argv) < 2:
-    print('provide argrument with list type')
     loginHandler()
+    print('provide argrument with list type')
     exit()
 
 if (sys.argv[1] in list_types) == False:
      print('Incorrect list type')
      exit()
     
-
-
 loginHandler()
 time.sleep(5)
 clickByID(elementIdList[0])
@@ -78,5 +101,10 @@ time.sleep(5)
 download(sys.argv[1])
 time.sleep(10)
 browser.close()
+if (sys.argv[1] == 'ter'):
+   unzipDownoladed()
+   time.sleep(2)
+   clearDirectory()
+
 
 
